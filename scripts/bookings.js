3006a7b51258ad1b062537154de9dc38b84d29fb
William Woodard <williamwoodard98@gmail.com>
2024-03-07 10:46:33 -0600

    bookings list

--- scripts/bookings.js
+++ scripts/bookings.js
@@ -2,15 +2,35 @@ import { getBookings, getBands, getVenues } from "./database.js";
 
 const bookings = getBookings()
 const venues = getVenues()
+const bands = getBands()
+
 
 export const BookingList = () => {
-    let html = "<ul>"
 
+    let html = "<ul>"
+    
+
+    // The large forof loop iterates through each booking
     for (const booking of bookings) {
+
+        
+        // This nested forof loop iterates through the bandIds and concats a string
+        let bandBookingHTML = ""
+        for (const bandId of booking.bandId) {           
+            for (const band of bands) {
+                if (band.id === bandId) {
+                    bandBookingHTML += `and ${band.name} `
+                }
+            }
+        }
+
+
+
+        // This nested forof loop iterates through the venueIds and concats a string of bands, venues, and bookingdates in a li
         for (const venue of venues) {
             if (booking.venueId === venue.id)
             html += `<li>
-            ${venue.name} are playing at the ${venue.name} on ${booking.date}
+            ${bandBookingHTML.slice(4)} are playing at the ${venue.name} on ${booking.date}
             </li>`
             
         }
